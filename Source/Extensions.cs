95491dde4f8ff485c6139c495843fdfb7cb0d892
DominikNITA <36695471+DominikNITA@users.noreply.github.com>
2021-02-08 16:51:46 +0100

    Version 2.0.0 preview - completly rewritten - new sample project - upgarde to .NET 5

---
+++ Source/Extensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dodo.BlazorThemeSwitcher
+{
+    public static class Extensions
+    {
+        public static IServiceCollection AddThemeSwitcher(this IServiceCollection serviceCollection, ICollection<string> themes)
+        {
+            return serviceCollection.AddSingleton(new ThemeManager(themes));
+        }
+
+        public static IServiceCollection AddThemeSwitcher(this IServiceCollection serviceCollection)
+        {
+            return serviceCollection.AddSingleton(new ThemeManager(new string[] { "light", "dark" }));
+        }
+    }
+}
---
+++ Source/ThemeManager.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dodo.BlazorThemeSwitcher
+{
+    public class ThemeManager
+    {
+        public event Action ThemeChanged;
+        public string[] Themes { get => _themes; private set => _themes = value; }
+        private string[] _themes;
+        int _currentThemeIndex;
+
+        public ThemeManager(ICollection<string> themes)
+        {
+            Themes = themes.ToArray();
+        }
+
+        public string GetCurrentTheme()
+        {
+            return Themes[_currentThemeIndex];
+        }
+
+        /// <summary>
+        /// Switch theme automatically to the next one
+        /// </summary>
+        /// <returns>New selected theme name</returns>
+        public string SwitchTheme()
+        {
+            _currentThemeIndex += 1;
+            _currentThemeIndex %= Themes.Length;
+            CallThemeChange();
+            return GetCurrentTheme();
+        }
+
+        /// <summary>
+        /// Switch theme automatically to the one passed in parameter
+        /// </summary>
+        /// <returns>Currently selected theme name</returns>
+        public string SwitchTheme(string theme)
+        {
+            int i = Array.IndexOf(Themes, theme);
+            if (i > 0)
+            {
+                _currentThemeIndex = i;
+            }
+            else
+            {
+                Console.Error.WriteLine($"Theme {theme} is not found!");
+            }
+            CallThemeChange();
+            return GetCurrentTheme();
+        }
+        private void CallThemeChange()
+        {
+            ThemeChanged?.Invoke();
+        }
+    }
+}
